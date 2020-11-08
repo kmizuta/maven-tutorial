2f6b045788c68bc173c4f1e6290a57ace7ecd811
Ken Mizuta <ken.mizuta@gmail.com>
2020-11-08 13:13:27 -0800

    Some initial modules

---
+++ common/file-util/src/main/java/us/mizuta/file/Util.java
@@ -0,0 +1,13 @@
+package us.mizuta.file;
+
+import java.nio.file.Path;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class Util {
+
+    public Stream<Path> find(Path directory, Function<Path, Boolean> isMatch) {
+        return null;
+    }
+
+}
